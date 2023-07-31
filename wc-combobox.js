cc6a715dbabd466e7384d6cb6dd53eefcc12945e
George Greene <georgegreene@outlook.com>
2023-07-30 22:33:09 -0400

    combobox: filter datalist based on user input, reset datalist on 'x' click and backspacing/deleting text, remove unused code;

--- wc-combobox.js
+++ wc-combobox.js
@@ -1,15 +1,11 @@
 class Combobox extends HTMLElement {
-  button;
   datalist;
   datalistNodes = [];
-  details;
   fieldset;
   input;
   label;
   li;
   shadowRoot;
-  span;
-  summary;
   ul;
 
   constructor() {
@@ -26,30 +22,22 @@ class Combobox extends HTMLElement {
   }
 
   createElements() {
-    this.button = document.createElement("button");
     this.datalist = document.createElement("datalist");
-    this.details = document.createElement("details");
     this.fieldset = document.createElement("fieldset");
     this.input = document.createElement("input");
     this.label = document.createElement("label");
     this.li = document.createElement("li");
-    this.span = document.createElement("span");
-    this.summary = document.createElement("summary");
     this.ul = document.createElement("ul");
   }
 
   applyAttributes() {
-    this.button.setAttribute("type", "checkbox");
     this.input.setAttribute("placeholder", "Type to search...");
     this.input.setAttribute("type", "search");
   }
 
   applyStyes() {
-    this.button.style.height = "20px";
-    this.button.style.width = "20px";
     this.datalist.style.display = "block";
     this.datalist.style.border = "1px solid";
-    this.details.style.backgroundColor = "violet";
     this.fieldset.style.display = "block";
     this.input.style.display = "block";
     this.input.style.width = "100%";
@@ -59,26 +47,22 @@ class Combobox extends HTMLElement {
   setContent() {
     this.label.textContent = this.getAttribute("label");
     this.li.textContent = "list item";
-    this.summary.textContent = "Click to expand";
   }
 
   appendChildren() {
     this.fieldset.appendChild(this.label);
     this.fieldset.appendChild(this.input);
-    this.fieldset.appendChild(this.datalist);
 
     this.datalist.appendChild(this.ul);
     this.ul.replaceChildren(...this.datalistNodes);
-    this.details.appendChild(this.summary);
-    this.details.appendChild(this.button);
 
     this.shadowRoot.appendChild(this.fieldset);
   }
 
   addEventListeners() {
-    this.input.addEventListener("focusin", this.expandDatalist);
-    this.input.addEventListener("focusout", this.collapseDatalist);
-    this.input.addEventListener("keyup", this.filterDatalist);
+    this.input.addEventListener("focus", this.expandDatalist);
+    this.input.addEventListener("blur", this.collapseDatalist);
+    this.input.addEventListener("input", this.handleSearchInput);
   }
 
   setListOptions() {
@@ -89,12 +73,40 @@ class Combobox extends HTMLElement {
     }
   }
 
-  expandDatalist = () => this.fieldset.appendChild(this.datalist);
-  collapseDatalist = () => this.fieldset.removeChild(this.datalist);
-  filterDatalist = (e) => {
-    console.log(e.target.value.toUpperCase());
-    console.log(this.datalistNodes);
-    console.log(this.ul.children);
+  handleSearchInput = (e) => {
+    if (!e.inputType && e.type === "input") {
+      this.datalist.replaceChildren(...this.datalistNodes);
+    } else if (
+      e.inputType === "insertText" ||
+      e.inputType === "deleteContentBackward" ||
+      e.inputType === "deleteContentForward"
+    ) {
+      this.filterDatalist(e.target.value);
+    }
+  };
+
+  expandDatalist = () => {
+    this.fieldset.appendChild(this.datalist);
+  };
+
+  collapseDatalist = () => {
+    this.fieldset.removeChild(this.datalist);
+  };
+
+  filterDatalist = (searchValue) => {
+    if (typeof searchValue === "string" && searchValue !== "") {
+      const matchingNodes = [];
+
+      for (let i = 0; i < this.datalistNodes.length; i++) {
+        if (this.datalistNodes[i].textContent === searchValue.toUpperCase()) {
+          matchingNodes.push(this.datalistNodes[i]);
+        }
+      }
+
+      this.datalist.replaceChildren(...matchingNodes);
+    } else if (typeof searchValue === "string" && searchValue === "") {
+      this.datalist.replaceChildren(...this.datalistNodes);
+    }
   };
 }
 
