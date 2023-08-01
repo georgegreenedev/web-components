65dcd57199918ad33390a6004ae344ee914a891e
George Greene <georgegreene@outlook.com>
2023-07-31 20:13:59 -0400

    combobox: replace alphabet with split lorem ipsum text in datalist, add simple overlow scrolling, change strict equality check with includes method for text input;

--- wc-combobox.js
+++ wc-combobox.js
@@ -36,8 +36,9 @@ class Combobox extends HTMLElement {
   }
 
   applyStyes() {
-    this.datalist.style.display = "block";
     this.datalist.style.border = "1px solid";
+    this.datalist.style.display = "block";
+    this.datalist.style.overflow = "scroll";
     this.fieldset.style.display = "block";
     this.input.style.display = "block";
     this.input.style.width = "100%";
@@ -66,9 +67,14 @@ class Combobox extends HTMLElement {
   }
 
   setListOptions() {
-    for (let i = 0; i < 26; i++) {
+    const loremIpsum =
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+    const loremIpsumList = loremIpsum.split(" ");
+
+    for (let i = 0; i < loremIpsumList.length; i++) {
       const option = document.createElement("li");
-      option.textContent = String.fromCharCode(i + 65);
+      option.textContent = loremIpsumList[i];
       this.datalistNodes.push(option);
     }
   }
@@ -98,7 +104,7 @@ class Combobox extends HTMLElement {
       const matchingNodes = [];
 
       for (let i = 0; i < this.datalistNodes.length; i++) {
-        if (this.datalistNodes[i].textContent === searchValue.toUpperCase()) {
+        if (this.datalistNodes[i].textContent.includes(searchValue)) {
           matchingNodes.push(this.datalistNodes[i]);
         }
       }
